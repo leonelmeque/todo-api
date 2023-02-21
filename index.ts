46048a4b20101ced022c70593468c132d394e324
Leonel Meque <leomeque@gmail.com>
2023-02-21 23:13:52 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import todosRouter from './src/routes/todos.route';
+import timelineRouter from './src/routes/timeline.route';
+import { logger } from './src/middleware/logger';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.use(logger);
+app.use('/api/todos', todosRouter);
+app.use('/api/timeline', timelineRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
+
