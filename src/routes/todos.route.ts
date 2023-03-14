47d9fc55e85b64fc6dcc6152f2c66cc849c0c72d
Leonel Meque <leomeque@gmail.com>
2023-03-14 08:36:54 +0100

    fix: todo api routes

--- src/routes/todos.route.ts
+++ src/routes/todos.route.ts
@@ -1,63 +1,23 @@
 import "colors";
 import express from "express";
-import * as todoServices from "../services/todos.service";
+import {
+  deleteTodo,
+  getMultipleTodos,
+  getTodoById,
+  saveTodos,
+  updateTodo,
+} from "../services/todos.service";
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-  const data = todoServices.getTodos();
-  res.status(200).send(data);
-});
+router.get("/find/:id", getTodoById);
 
-router.get("/find/:id", (req, res) => {
-  const data = todoServices.getTodoById(req.params.id);
+router.post("/find-multiple", getMultipleTodos);
 
-  res.send({
-    result: data,
-  });
-});
+router.post("/save", saveTodos);
 
-router.get("/all", (req, res) => {
-  const arr = (req.query.ids as string).replace(" ", "").split(/,/g);
-  console.log(arr);
-  const data = todoServices.getAllTodosById(arr);
+router.put("/update/:id", updateTodo);
 
-  res.status(200).send({
-    message: "List of all todos",
-    result: data || [],
-  });
-});
-
-router.post("/save", (req, res) => {
-  todoServices.saveTodo(req.body);
-
-  res.status(200).send({
-    message: "Todo saved!",
-    todo: req.body,
-  });
-});
-
-router.put("/update/:id", (req, res) => {
-  const data = todoServices.updateTodos(req.params.id, req.body);
-
-  res.status(200).send({
-    message: "Todo has been updated!",
-    todo: data,
-  });
-});
-
-router.delete("/remove/:id", (req, res) => {
-  const result = todoServices.deleteTodo(req.params.id);
-
-  if (result) {
-    res.status(200).send({
-      message: "Todo removed!",
-    });
-  } else {
-    res.status(404).send({
-      message: "Todo not found!",
-    });
-  }
-});
+router.delete("/remove/:id", deleteTodo);
 
 export default router;
