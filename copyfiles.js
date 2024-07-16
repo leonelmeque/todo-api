58f3c6552c9ef8ba6f8334a20df58e98f8f2e6e4
Leonel Meque <leomeque@gmail.com>
2024-07-17 01:21:23 +0200

    [no-ticket] feat: copy mock data for staging

---
+++ copyfiles.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const copyFile = (source, target) => { const targetDir = path.dirname(target);
+
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+  }
+
+  fs.copyFileSync(source, target); };
+const assetsPath = path.join(__dirname, 'src', 'db');
+const distPath = path.join(__dirname, 'dist', 'db');
+
+fs.readdirSync(assetsPath)
+  .forEach(file => {
+  if (file.endsWith('.json')) {
+    copyFile(path.join(assetsPath, file), path.join(distPath, file));
+    }
+   }
+  );
