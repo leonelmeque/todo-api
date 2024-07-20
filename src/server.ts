7a5daea3e686e645c576461ac92cde9365cfbcfa
Leonel Meque <leomeque@gmail.com>
2024-07-21 00:33:24 +0200

    [no-ticket] refactor: add import dummy data when DEV/STAGE ENV

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import app from "./app";
 
 const PORT = process.env.PORT || 3000
+const ENVIRONMENT = process.env.ENV
+
+if(ENVIRONMENT === "DEV" || ENVIRONMENT === "STAGE" ) {
+  import('../scripts/upload-dummy-data')
+}
 
 app.listen(PORT, ()=> {
   console.log("Server is listening on port " + PORT)
