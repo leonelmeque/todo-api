1ee50028ded7b561da6531f8c93f26bb016782de
Leonel Meque <leomeque@gmail.com>
2024-08-18 22:22:59 +0200

    [no-ticket] refactor: app file

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,32 @@ import path from  'path'
 import cookieParser from  'cookie-parser'
 import logger from  'morgan'
 import { verifyToken } from './middleware/auth.middleware';
-
-
+import cors from 'cors';
 import usersRouter from './api/user/user.routes'
 import todosRouter from './api/todos/todo.routes'
 import authRouter from './api/auth/auth.routes'
+import { constants, corsOptions, endpoints, rateLimiter } from './utils/';
 
-let app = express();
+const {API_PREFIX, API_VERSION} = constants
+const app = express();
 
-const API_PREFIX = '/api';
-const API_VERSION = '/v1';
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(API_PREFIX + '/', rateLimiter)
+app.use('/api/test', endpoints.test)
+
+if(process.env.ENV === 'DEV') {
+  app.use('/reset-limit', endpoints.resetLimit)
+}
 
 app.use('/auth', authRouter)
 app.use(`${API_PREFIX}${API_VERSION}/todos`, verifyToken, todosRouter)
